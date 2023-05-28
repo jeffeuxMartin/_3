024cb700abee9e6300c6b2e5cc696a82e47af583
Jeff Chen <r09942097@ntu.edu.tw>
2023-05-29 01:17:49 +0800

    First try here一  5月 29 01:17:49 CST 2023

---
+++ bojun/_t1.sh
@@ -0,0 +1,124 @@
+#!sh
+
+tmux new-sess -d ncdu -o '/home/jeffeux/Desktop/bojun/Alarms' \
+'/run/user/1000/gvfs/mtp:host=%5Busb%3A001%2C021%5D/內部共用儲存空間/Alarms'
+tmux new-sess -d ncdu -o '/home/jeffeux/Desktop/bojun/Android' \
+'/run/user/1000/gvfs/mtp:host=%5Busb%3A001%2C021%5D/內部共用儲存空間/Android'
+tmux new-sess -d ncdu -o '/home/jeffeux/Desktop/bojun/AndroMoney' \
+'/run/user/1000/gvfs/mtp:host=%5Busb%3A001%2C021%5D/內部共用儲存空間/AndroMoney'
+tmux new-sess -d ncdu -o '/home/jeffeux/Desktop/bojun/antifraud' \
+'/run/user/1000/gvfs/mtp:host=%5Busb%3A001%2C021%5D/內部共用儲存空間/antifraud'
+tmux new-sess -d ncdu -o '/home/jeffeux/Desktop/bojun/AnyDesk' \
+'/run/user/1000/gvfs/mtp:host=%5Busb%3A001%2C021%5D/內部共用儲存空間/AnyDesk'
+tmux new-sess -d ncdu -o '/home/jeffeux/Desktop/bojun/Audiobooks' \
+'/run/user/1000/gvfs/mtp:host=%5Busb%3A001%2C021%5D/內部共用儲存空間/Audiobooks'
+tmux new-sess -d ncdu -o '/home/jeffeux/Desktop/bojun/backup' \
+'/run/user/1000/gvfs/mtp:host=%5Busb%3A001%2C021%5D/內部共用儲存空間/backup'
+tmux new-sess -d ncdu -o '/home/jeffeux/Desktop/bojun/backups' \
+'/run/user/1000/gvfs/mtp:host=%5Busb%3A001%2C021%5D/內部共用儲存空間/backups'
+tmux new-sess -d ncdu -o '/home/jeffeux/Desktop/bojun/bluetooth' \
+'/run/user/1000/gvfs/mtp:host=%5Busb%3A001%2C021%5D/內部共用儲存空間/bluetooth'
+tmux new-sess -d ncdu -o '/home/jeffeux/Desktop/bojun/bookcase_json' \
+'/run/user/1000/gvfs/mtp:host=%5Busb%3A001%2C021%5D/內部共用儲存空間/bookcase_json'
+tmux new-sess -d ncdu -o '/home/jeffeux/Desktop/bojun/Cardboard' \
+'/run/user/1000/gvfs/mtp:host=%5Busb%3A001%2C021%5D/內部共用儲存空間/Cardboard'
+tmux new-sess -d ncdu -o '/home/jeffeux/Desktop/bojun/com.facebook.katana' \
+'/run/user/1000/gvfs/mtp:host=%5Busb%3A001%2C021%5D/內部共用儲存空間/com.facebook.katana'
+tmux new-sess -d ncdu -o '/home/jeffeux/Desktop/bojun/com.facebook.orca' \
+'/run/user/1000/gvfs/mtp:host=%5Busb%3A001%2C021%5D/內部共用儲存空間/com.facebook.orca'
+tmux new-sess -d ncdu -o '/home/jeffeux/Desktop/bojun/com.game.abroad' \
+'/run/user/1000/gvfs/mtp:host=%5Busb%3A001%2C021%5D/內部共用儲存空間/com.game.abroad'
+tmux new-sess -d ncdu -o '/home/jeffeux/Desktop/bojun/com.rise.automatic.autoclicker.clicker' \
+'/run/user/1000/gvfs/mtp:host=%5Busb%3A001%2C021%5D/內部共用儲存空間/com.rise.automatic.autoclicker.clicker'
+tmux new-sess -d ncdu -o '/home/jeffeux/Desktop/bojun/cwmoney' \
+'/run/user/1000/gvfs/mtp:host=%5Busb%3A001%2C021%5D/內部共用儲存空間/cwmoney'
+tmux new-sess -d ncdu -o '/home/jeffeux/Desktop/bojun/DCIM' \
+'/run/user/1000/gvfs/mtp:host=%5Busb%3A001%2C021%5D/內部共用儲存空間/DCIM'
+tmux new-sess -d ncdu -o '/home/jeffeux/Desktop/bojun/Documents' \
+'/run/user/1000/gvfs/mtp:host=%5Busb%3A001%2C021%5D/內部共用儲存空間/Documents'
+tmux new-sess -d ncdu -o '/home/jeffeux/Desktop/bojun/Download' \
+'/run/user/1000/gvfs/mtp:host=%5Busb%3A001%2C021%5D/內部共用儲存空間/Download'
+tmux new-sess -d ncdu -o '/home/jeffeux/Desktop/bojun/DroidScript' \
+'/run/user/1000/gvfs/mtp:host=%5Busb%3A001%2C021%5D/內部共用儲存空間/DroidScript'
+tmux new-sess -d ncdu -o '/home/jeffeux/Desktop/bojun/Easy Screen Recorder' \
+'/run/user/1000/gvfs/mtp:host=%5Busb%3A001%2C021%5D/內部共用儲存空間/Easy Screen Recorder'
+tmux new-sess -d ncdu -o '/home/jeffeux/Desktop/bojun/Edited' \
+'/run/user/1000/gvfs/mtp:host=%5Busb%3A001%2C021%5D/內部共用儲存空間/Edited'
+tmux new-sess -d ncdu -o '/home/jeffeux/Desktop/bojun/EhViewer' \
+'/run/user/1000/gvfs/mtp:host=%5Busb%3A001%2C021%5D/內部共用儲存空間/EhViewer'
+tmux new-sess -d ncdu -o '/home/jeffeux/Desktop/bojun/FFMPEG' \
+'/run/user/1000/gvfs/mtp:host=%5Busb%3A001%2C021%5D/內部共用儲存空間/FFMPEG'
+tmux new-sess -d ncdu -o '/home/jeffeux/Desktop/bojun/Icode-Go' \
+'/run/user/1000/gvfs/mtp:host=%5Busb%3A001%2C021%5D/內部共用儲存空間/Icode-Go'
+tmux new-sess -d ncdu -o '/home/jeffeux/Desktop/bojun/inCollage' \
+'/run/user/1000/gvfs/mtp:host=%5Busb%3A001%2C021%5D/內部共用儲存空間/inCollage'
+tmux new-sess -d ncdu -o '/home/jeffeux/Desktop/bojun/Jeff' \
+'/run/user/1000/gvfs/mtp:host=%5Busb%3A001%2C021%5D/內部共用儲存空間/Jeff'
+tmux new-sess -d ncdu -o '/home/jeffeux/Desktop/bojun/JeffScripter' \
+'/run/user/1000/gvfs/mtp:host=%5Busb%3A001%2C021%5D/內部共用儲存空間/JeffScripter'
+tmux new-sess -d ncdu -o '/home/jeffeux/Desktop/bojun/jorte' \
+'/run/user/1000/gvfs/mtp:host=%5Busb%3A001%2C021%5D/內部共用儲存空間/jorte'
+tmux new-sess -d ncdu -o '/home/jeffeux/Desktop/bojun/log' \
+'/run/user/1000/gvfs/mtp:host=%5Busb%3A001%2C021%5D/內部共用儲存空間/log'
+tmux new-sess -d ncdu -o '/home/jeffeux/Desktop/bojun/Lua Scripting' \
+'/run/user/1000/gvfs/mtp:host=%5Busb%3A001%2C021%5D/內部共用儲存空間/Lua Scripting'
+tmux new-sess -d ncdu -o '/home/jeffeux/Desktop/bojun/MediBangPaint' \
+'/run/user/1000/gvfs/mtp:host=%5Busb%3A001%2C021%5D/內部共用儲存空間/MediBangPaint'
+tmux new-sess -d ncdu -o '/home/jeffeux/Desktop/bojun/MicTest' \
+'/run/user/1000/gvfs/mtp:host=%5Busb%3A001%2C021%5D/內部共用儲存空間/MicTest'
+tmux new-sess -d ncdu -o '/home/jeffeux/Desktop/bojun/Mobizen' \
+'/run/user/1000/gvfs/mtp:host=%5Busb%3A001%2C021%5D/內部共用儲存空間/Mobizen'
+tmux new-sess -d ncdu -o '/home/jeffeux/Desktop/bojun/Movies' \
+'/run/user/1000/gvfs/mtp:host=%5Busb%3A001%2C021%5D/內部共用儲存空間/Movies'
+tmux new-sess -d ncdu -o '/home/jeffeux/Desktop/bojun/Music' \
+'/run/user/1000/gvfs/mtp:host=%5Busb%3A001%2C021%5D/內部共用儲存空間/Music'
+tmux new-sess -d ncdu -o '/home/jeffeux/Desktop/bojun/Notesnook' \
+'/run/user/1000/gvfs/mtp:host=%5Busb%3A001%2C021%5D/內部共用儲存空間/Notesnook'
+tmux new-sess -d ncdu -o '/home/jeffeux/Desktop/bojun/Notifications' \
+'/run/user/1000/gvfs/mtp:host=%5Busb%3A001%2C021%5D/內部共用儲存空間/Notifications'
+tmux new-sess -d ncdu -o '/home/jeffeux/Desktop/bojun/PDFSaved' \
+'/run/user/1000/gvfs/mtp:host=%5Busb%3A001%2C021%5D/內部共用儲存空間/PDFSaved'
+tmux new-sess -d ncdu -o '/home/jeffeux/Desktop/bojun/Pictures' \
+'/run/user/1000/gvfs/mtp:host=%5Busb%3A001%2C021%5D/內部共用儲存空間/Pictures'
+tmux new-sess -d ncdu -o '/home/jeffeux/Desktop/bojun/Podcasts' \
+'/run/user/1000/gvfs/mtp:host=%5Busb%3A001%2C021%5D/內部共用儲存空間/Podcasts'
+tmux new-sess -d ncdu -o '/home/jeffeux/Desktop/bojun/ProfilePassport' \
+'/run/user/1000/gvfs/mtp:host=%5Busb%3A001%2C021%5D/內部共用儲存空間/ProfilePassport'
+tmux new-sess -d ncdu -o '/home/jeffeux/Desktop/bojun/qlua5' \
+'/run/user/1000/gvfs/mtp:host=%5Busb%3A001%2C021%5D/內部共用儲存空間/qlua5'
+tmux new-sess -d ncdu -o '/home/jeffeux/Desktop/bojun/QooApp' \
+'/run/user/1000/gvfs/mtp:host=%5Busb%3A001%2C021%5D/內部共用儲存空間/QooApp'
+tmux new-sess -d ncdu -o '/home/jeffeux/Desktop/bojun/Recordings' \
+'/run/user/1000/gvfs/mtp:host=%5Busb%3A001%2C021%5D/內部共用儲存空間/Recordings'
+tmux new-sess -d ncdu -o '/home/jeffeux/Desktop/bojun/recovery' \
+'/run/user/1000/gvfs/mtp:host=%5Busb%3A001%2C021%5D/內部共用儲存空間/recovery'
+tmux new-sess -d ncdu -o '/home/jeffeux/Desktop/bojun/Ringtones' \
+'/run/user/1000/gvfs/mtp:host=%5Busb%3A001%2C021%5D/內部共用儲存空間/Ringtones'
+tmux new-sess -d ncdu -o '/home/jeffeux/Desktop/bojun/ScreenRecords' \
+'/run/user/1000/gvfs/mtp:host=%5Busb%3A001%2C021%5D/內部共用儲存空間/ScreenRecords'
+tmux new-sess -d ncdu -o '/home/jeffeux/Desktop/bojun/Server_Auditor_Temp' \
+'/run/user/1000/gvfs/mtp:host=%5Busb%3A001%2C021%5D/內部共用儲存空間/Server_Auditor_Temp'
+tmux new-sess -d ncdu -o '/home/jeffeux/Desktop/bojun/shopeeTW' \
+'/run/user/1000/gvfs/mtp:host=%5Busb%3A001%2C021%5D/內部共用儲存空間/shopeeTW'
+tmux new-sess -d ncdu -o '/home/jeffeux/Desktop/bojun/Snapask' \
+'/run/user/1000/gvfs/mtp:host=%5Busb%3A001%2C021%5D/內部共用儲存空間/Snapask'
+tmux new-sess -d ncdu -o '/home/jeffeux/Desktop/bojun/SonarPen' \
+'/run/user/1000/gvfs/mtp:host=%5Busb%3A001%2C021%5D/內部共用儲存空間/SonarPen'
+tmux new-sess -d ncdu -o '/home/jeffeux/Desktop/bojun/Subtitles' \
+'/run/user/1000/gvfs/mtp:host=%5Busb%3A001%2C021%5D/內部共用儲存空間/Subtitles'
+tmux new-sess -d ncdu -o '/home/jeffeux/Desktop/bojun/super_recorder' \
+'/run/user/1000/gvfs/mtp:host=%5Busb%3A001%2C021%5D/內部共用儲存空間/super_recorder'
+tmux new-sess -d ncdu -o '/home/jeffeux/Desktop/bojun/Telegram' \
+'/run/user/1000/gvfs/mtp:host=%5Busb%3A001%2C021%5D/內部共用儲存空間/Telegram'
+tmux new-sess -d ncdu -o '/home/jeffeux/Desktop/bojun/tencent' \
+'/run/user/1000/gvfs/mtp:host=%5Busb%3A001%2C021%5D/內部共用儲存空間/tencent'
+tmux new-sess -d ncdu -o '/home/jeffeux/Desktop/bojun/TJA' \
+'/run/user/1000/gvfs/mtp:host=%5Busb%3A001%2C021%5D/內部共用儲存空間/TJA'
+tmux new-sess -d ncdu -o '/home/jeffeux/Desktop/bojun/txrtmp' \
+'/run/user/1000/gvfs/mtp:host=%5Busb%3A001%2C021%5D/內部共用儲存空間/txrtmp'
+tmux new-sess -d ncdu -o '/home/jeffeux/Desktop/bojun/vFlat' \
+'/run/user/1000/gvfs/mtp:host=%5Busb%3A001%2C021%5D/內部共用儲存空間/vFlat'
+tmux new-sess -d ncdu -o '/home/jeffeux/Desktop/bojun/vrecorder' \
+'/run/user/1000/gvfs/mtp:host=%5Busb%3A001%2C021%5D/內部共用儲存空間/vrecorder'
+tmux new-sess -d ncdu -o '/home/jeffeux/Desktop/bojun/X Video Player' \
+'/run/user/1000/gvfs/mtp:host=%5Busb%3A001%2C021%5D/內部共用儲存空間/X Video Player'
